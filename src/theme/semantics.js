322e4dd07fc02a6a5a70cc3d6ede9a58dfc3a208
seulbinim <skfo0827@naver.com>
2024-12-12 11:37:19 +0900

    🐛fix: 다크모드 ESLint 경고

--- src/theme/semantics.js
+++ src/theme/semantics.js
@@ -7,6 +7,7 @@ export const THEME_MODE = {
   DARK: 'dark',
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   [THEME_MODE.LIGHT]: {
     forground: color.gray[800],
