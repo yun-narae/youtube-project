1109bc383e1235a1d8bba79d3f0bb0af9d561ffc
seulbinim <skfo0827@naver.com>
2024-12-16 18:58:38 +0900

    🎨feat: 메인 콘텐츠 세팅

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer id='footer' role='contentinfo'>
-        Footer
-    </footer>
-  )
+    return (
+        <footer id='footer' role="contentinfo"> 
+            <a href="mailto:webstoryboy@naver.com" rel="noopener noreferrer">
+                webstoryboy@naver.com
+            </a>
+        </footer>
+    )
 }
 
 export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
+import Today from '../components/contents/Today';
+import Developer from '../components/contents/Developer';
+import Webd from '../components/contents/Webd';
+import Website from '../components/contents/Website';
+import Gsap from '../components/contents/Gsap';
+import Portfolio from '../components/contents/Portfolio';
+import Youtube from '../components/contents/Youtube';
+
 const Home = () => {
     return (
         <Main
             title="웹스토리보이 유튜브"
             description="웹스토리보이 유튜브에 오신것을 환영합니다."
         >
-            Home
+            <Today />
+            <Developer />
+            <Webd />
+            <Website />
+            <Gsap />
+            <Portfolio />
+            <Youtube />
+
         </Main>
     )
 }
