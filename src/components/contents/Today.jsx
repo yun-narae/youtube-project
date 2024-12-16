1c122c1695a694523a0e23fe9e0a2a762cf7dc7b
seulbinim <skfo0827@naver.com>
2024-12-16 19:45:18 +0900

    🎨feat: 추천 영상 페이지 작업 - 스타일링 - 텍스트 관련 믹스인

--- src/components/contents/Today.jsx
+++ src/components/contents/Today.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { todayText } from '../../data/Today'
 
 const Today = () => {
   return (
-    <div>Today</div>
+    <section id='today'>
+        <div className='today__inner'>
+            <div className='today__thumb'>
+                <Link to={todayText[0].page}>
+                    <img src={todayText[0].img} alt={todayText[0].title} />
+                </Link>
+            </div>
+            <div className='today__text'>
+                <span className='today'>today</span>
+                <Link to={todayText[0].page}>
+                    <h3 className='title'>{todayText[0].title}</h3>
+                    <p className='desc'>{todayText[0].desc}</p>
+                </Link>
+                <div className='info'>
+                    <Link to={`/channel/${todayText[0].channelId}`}>
+                        <span className='author'>{todayText[0].author}</span>
+                    </Link>
+                    <span className='date'>{todayText[0].date}</span>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import { todayText } from '../data/Today'
+import { Link } from 'react-router-dom'
 
 const Today = () => {
     return (
@@ -7,7 +9,31 @@ const Today = () => {
             title = "추천 영상"
             description="오늘의 추천 유튜브 영상입니다."
         >
-            Today
+            <section id='todayPage'>
+                <h2>오늘의 추천영상</h2>
+                {todayText.map((today, key) => (
+                    <div className='today__inner' key={key}>
+                        <div className='today__thumb'>
+                            <Link to={today.page}>
+                                <img src={today.img} alt={today.title} />
+                            </Link>
+                        </div>
+                        <div className='today__text'>
+                            <span className='today'>today</span>
+                            <Link to={today.page}>
+                                <h3 className='title'>{today.title}</h3>
+                                <p className='desc'>{today.desc}</p>
+                            </Link>
+                            <div className='info'>
+                                <Link to={`/channel/${today.channelId}`}>
+                                    <span className='author'>{today.author}</span>
+                                </Link>
+                                <span className='date'>{today.date}</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </section>
         </Main>
     )
 }
