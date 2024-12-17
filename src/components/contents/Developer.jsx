d96600788d5f1c48d603bdd3ae9fd635506bccb0
seulbinim <skfo0827@naver.com>
2024-12-17 15:01:56 +0900

    🎨feat: 메인페이지에 추천 개발자 세팅

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { developerText } from '../../data/developer'
+
 
 const Developer = () => {
-  return (
-    <div>Developer</div>
-  )
+    return (
+        <section id='developer'>
+            <h2>추천 개발자</h2>
+            <div className='developer__inner overflow'>
+                {developerText.map((developer, key) => 
+                    <div className='developer' key={key}>
+                            <div className="developer__img play__icon">
+                                <Link to={`/channel/${developer.channelId}`}>
+                                        <img src={developer.img} alt={developer.name} />
+                                </Link>
+                            </div>
+                            <Link to={`/channel/${developer.channelId}`}>
+                                <p>{developer.name}</p>
+                            </Link>
+                    </div>
+                )}
+            </div>
+        </section>
+    )
 }
 
 export default Developer
