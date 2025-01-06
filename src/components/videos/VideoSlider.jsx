ef75c3cf4b0491fbf616c0f5a7fde17ccf5fb43d
seulbinim <skfo0827@naver.com>
2025-01-06 14:05:03 +0900

    충돌수정

--- src/components/videos/VideoSlider.jsx
+++ src/components/videos/VideoSlider.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
