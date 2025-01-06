ff2b151b7c09af370736ff3a932df54f749b88b4
seulbinim <skfo0827@naver.com>
2025-01-06 11:18:38 +0900

    Swiper 작업

--- src/components/videos/VideoSlider.jsx
+++ src/components/videos/VideoSlider.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 
-
 const VideoSlider = ({ videos, title, id }) => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 500) 
+    }, []);
+    // isLoading에서 0.5초 후에 isLoaded로 바뀐다.
+    // 실제 데이터 로딩 시간무관하고 애니메이션을 주기위한 작업
+    const videoClass = loading ? 'isLoading' : 'isLoaded';
+
     return (
-        <section id={id} >
+        <section id={id} className={videoClass}>
             <h2>{title}</h2>
             <div className='video__slider'>
             <Swiper 
@@ -17,6 +27,24 @@ const VideoSlider = ({ videos, title, id }) => {
                 navigation={true}
                 modules={[Navigation]}
                 className={`mySwiper-${id}`}
+                breakpoints={{
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 20
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 20
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 20
+                    },
+                    1600: {
+                        slidesPerView: 5,
+                        spaceBetween: 20
+                    }
+                }}
             >
                 {videos.map((video, key) => (
                     <SwiperSlide key={key}>
