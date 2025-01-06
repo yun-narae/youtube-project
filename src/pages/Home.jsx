254313977f33400561b2110cb71f65f05b8c3f4e
seulbinim <skfo0827@naver.com>
2025-01-06 09:49:44 +0900

    VideoSlider 컴포넌트 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import Today from '../components/contents/Today'
+import Developer from '../components/contents/Developer'
+import VideoSlider from '../components/videos/VideoSlider'
 
-import Today from '../components/contents/Today';
-import Developer from '../components/contents/Developer';
-import Webd from '../components/contents/Webd';
-import Website from '../components/contents/Website';
-import Gsap from '../components/contents/Gsap';
-import Portfolio from '../components/contents/Portfolio';
-import Youtube from '../components/contents/Youtube';
+import { webdText } from '../data/webd'
+import { websiteText } from '../data/website'
+import { gsapText } from '../data/gsap'
+import { portfolioText } from '../data/portfolio'
+import { youtubeText } from '../data/youtube'
 
 const Home = () => {
     return (
-        <Main
-            title="웹스토리보이 유튜브"
-            description="웹스토리보이 유튜브에 오신것을 환영합니다."
-        >
+        <Main 
+            title = "웹스토리보이 유튜브"
+            description="웹스토리보이 유튜버 사이트에 오신 것을 환영합니다.">
+            
             <Today />
             <Developer />
-            <Webd />
-            <Website />
-            <Gsap />
-            <Portfolio />
-            <Youtube />
-
+            <VideoSlider videos={webdText} title="😮 웹디자인기능사 준비는 이걸로!" id="webd" />
+            <VideoSlider videos={websiteText} title="😛 웹표준 사이트 만들기 기초 다지기" id="website" />
+            <VideoSlider videos={gsapText} title="🤓 GSAP 패럴랙스 효과를 하고 싶다면!" id="gsap" />
+            <VideoSlider videos={portfolioText} title="🤗 포트폴리오 만드는 방법을 공유합니다." id="portfolio" />
+            <VideoSlider videos={youtubeText} title="😱 지금 이 코딩을 영상으로" id="youtube" />
         </Main>
     )
 }
