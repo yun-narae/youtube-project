e81b128f054d75144fdc1b09d9f44f1fa6072ad7
seulbinim <skfo0827@naver.com>
2025-01-08 10:59:35 +0900

    오류 : videoId가 undefined

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
+import { useParams } from "react-router-dom";
 
 const Video = () => {
+    const { videoId } = useParams();
+    const { videoDetail, setVideoDetail } = useState(null);
+
+    useEffect(() => {
+        fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoId}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data)
+        })
+        console.log(videoId)
+    }, [videoId]);
+
     return (
         <Main 
             title = "유튜브 비디오 영상"
