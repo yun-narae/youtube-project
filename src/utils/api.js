9dceee9e32dd6f57546e5a0f4a8135ed9be1945f
seulbinim <skfo0827@naver.com>
2025-01-08 09:51:37 +0900

    RapidAPI

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,18 @@
-const axios = require('axios');
+import axios from "axios";
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
 const options = {
-  method: 'GET',
-  url: 'https://youtube-v31.p.rapidapi.com/search',
-  params: {
-    q: 'music',
-    part: 'snippet,id',
-    regionCode: 'US',
-    maxResults: '50',
-  },
-  headers: {
-    'x-rapidapi-key': '0357bc6437msh6db8e078013da2dp1ef052jsn332b55611064',
-    'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
-  }
+    params: {
+        maxResults: '50',
+    },
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
 };
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
+export const fetchFromAPI = async(url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
 }
