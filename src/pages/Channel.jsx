2b5f3894aba1d8f5489283de08b302bad542c71d
seulbinim <skfo0827@naver.com>
2025-01-09 12:16:52 +0900

    채널 상세페이지 구현 - 유튜브API에서의 채널 배너는 OAuth 2.0 인증이 필요

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { CiBadgeDollar, CiMedal, CiRead } from 'react-icons/ci';
 
 const Channel = () => {
@@ -23,25 +23,27 @@ const Channel = () => {
             title = "유튜브 채널"
             description="유튜브 채널페이지입니다."
         >
-            {channelDetail && (
-                <section id='channel'>
-                    <div className='channel__header'>
-                        {/* 채널 배너는 OAuth 2.0 인증이 필요 */}
-                        <div className='circle'>
-                            <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
+            <section id='channel'>
+                {channelDetail && (
+                    <div className='channel__inner'>
+                        <div className='channel__header'>
+                            {/* 채널 배너는 OAuth 2.0 인증이 필요 */}
+                            <div className='circle'>
+                                <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
+                            </div>
+                        </div>
+                        <div className='channel__info'>
+                            <h3 className='title'>{channelDetail.snippet.title}</h3>
+                            <p className='desc'>{channelDetail.snippet.description}</p>
+                            <div className='info'>
+                                <span><CiBadgeDollar />{channelDetail.statistics.subscriberCount}</span>
+                                <span><CiMedal />{channelDetail.statistics.videoCount}</span>
+                                <span><CiRead />{channelDetail.statistics.viewCount}</span>
+                            </div>
                         </div>
                     </div>
-                    <div className='channel__info'>
-                        <h3 className='title'>{channelDetail.snippet.title}</h3>
-                        <p className='desc'>{channelDetail.snippet.description}</p>
-                        <div className='info'>
-                            <span><CiBadgeDollar />{channelDetail.statistics.subscriberCount}</span>
-                            <span><CiMedal />{channelDetail.statistics.videoCount}</span>
-                            <span><CiRead />{channelDetail.statistics.viewCount}</span>
-                        </div>
-                    </div>
-                </section>
-            )}
+                )}
+            </section>
         </Main>
     )
 }
