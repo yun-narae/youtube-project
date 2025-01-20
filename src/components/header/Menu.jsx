939488498b63d9adeb4f6e948b81e0108aea2343
seulbinim <skfo0827@naver.com>
2025-01-20 13:24:12 +0900

    menu list hover 오류 수정 전

--- src/components/header/Menu.jsx
+++ src/components/header/Menu.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { headerMenus, searchKeyword } from '../../data/header';
 import { useTheme } from '../../contexts/theme';
@@ -8,6 +8,10 @@ import AppSwitch from '../AppSwitch';
 
 const Menu = () => {
     const { isDarkMode, toggleMode } = useTheme();
+    // 각 항목에 대한 hover 상태 배열 관리
+    const [menuHovered, setMenuHovered] = useState(new Array(headerMenus.length).fill(false)); // 메뉴 항목별 hover 상태
+    const [keywordHovered, setKeywordHovered] = useState(new Array(searchKeyword.length).fill(false)); // 키워드 항목별 hover 상태
+
 
     const location = useLocation();
 
@@ -18,39 +22,52 @@ const Menu = () => {
       }, [toggleMode]);
 
     const {
-        Header: { borderColor },
+        Header: { borderColor},
+        Navigation: { keywordBorderColor, activenavbackgroundColor, hovernavbackgroundColor, activeKeywordBorderColor, hoverkeywordBorderColor }
     } = theme;
-
-    const {
-        Navigation: { keywordBorderColor, activenavbackgroundColor, activeKeywordBorderColor },
-    } = theme;
-
-    const menuStyles = {
-        borderColor: borderColor,
-    };
-
-    const menuActiveStyles = {
-        backgroundColor: activenavbackgroundColor,
-    };
-
-    const keywordStyles = {
-        borderColor: keywordBorderColor,
-    };
-
-    const keywordActiveStyles = {
-        borderColor: activeKeywordBorderColor,
-    };
-
-    const containerStyles = {
-        borderColor: borderColor,
-    };
+    
+    const styles = {
+        menuStyles: {
+            borderColor: borderColor,
+        },
+        menuActiveStyles: {
+            backgroundColor: activenavbackgroundColor,
+        },
+        menuHoverStyles: {
+            backgroundColor: hovernavbackgroundColor,
+        },
+        keywordStyles: {
+            borderColor: keywordBorderColor,
+        },
+        keywordActiveStyles: {
+            borderColor: activeKeywordBorderColor,
+        },
+        keywordHoverStyles: {
+            borderColor: hoverkeywordBorderColor,
+        },
+        containerStyles: {
+            borderColor: borderColor,
+        }
+    }
 
     return (
         <nav className='header__menu'>
-            <ul className='menu' style={menuStyles}>
+            <ul className='menu' style={styles.menuStyles}>
                 {headerMenus.map((menu, key) => (
                     <li key={key}>
-                        <Link to={menu.src} style={location.pathname === menu.src ? menuActiveStyles : {}}>
+                        <Link 
+                            to={menu.src} 
+                            onMouseEnter={() => setMenuHovered(true)} 
+                            onMouseLeave={() => setMenuHovered(false)}
+                            style={{
+                                ...(
+                                    location.pathname === menu.src 
+                                        ? styles.menuActiveStyles
+                                        : styles.menuStyles
+                                ),
+                                ...(menuHovered ? styles.menuHoverStyles : {}), // hover 스타일 적용
+                            }}
+                        >
                             {menu.icon}{menu.title}
                         </Link>
                     </li>
@@ -59,13 +76,25 @@ const Menu = () => {
             <ul className='keyword' >
                 {searchKeyword.map((keyword, key) => (
                     <li key={key}>
-                        <Link to={keyword.src} style={location.pathname === keyword.src ? keywordActiveStyles : keywordStyles}>
+                        <Link 
+                            to={keyword.src}
+                            onMouseEnter={() => setKeywordHovered(true)} 
+                            onMouseLeave={() => setKeywordHovered(false)}
+                            style={{
+                                ...(
+                                    location.pathname === keyword.src 
+                                        ? styles.keywordActiveStyles 
+                                        : styles.keywordStyles
+                                ),
+                                ...(keywordHovered ? styles.keywordHoverStyles : {}), // hover 스타일 적용
+                            }}
+                        >
                             {keyword.title}
                         </Link>
                     </li>
                 ))}
             </ul>
-            <div style={containerStyles} className='switch'>
+            <div style={styles.containerStyles} className='switch'>
                 <AppSwitch value={isDarkMode} onToggle={handleToggle} ratio={2} />
                 {isDarkMode ? '다크' : '라이트'} 모드
             </div>
@@ -74,4 +103,4 @@ const Menu = () => {
     )
 }
 
-export default Menu
+export default Menu;
