a03e76c17c531b7b94d11a680b39b1fa45cfa6fc
narae <skfo0827@naver.com>
2025-10-07 18:57:10 +0900

    refactor: developerText 불필요 항목 삭제

--- src/data/developer.js
+++ src/data/developer.js
@@ -1,10 +1,4 @@
 export const developerText = [
-    // {
-    //     img: "https://yt3.googleusercontent.com/JU6k565OVstxx_h_7TbzE1aNXKzcxKl33zjPV4p649pqsgS7E3vb0meX9DB1_D_KviDoc4xM=s176-c-k-c0x00ffffff-no-rj",
-    //     name: "webstoryboy",
-    //     channelAddress: "https://www.youtube.com/@webstoryboy",
-    //     channelId: "UCsvQSv7EeCMHyYb9ENKAJZw"
-    // },
     {
         img: "https://yt3.ggpht.com/6gJoxwN9IQ3SlQIo1SibzhmwNLfxhX3t15zfQAGMxmV3Fi7aALzNayf2CzqhL5U6c2f4123G=s800-c-k-c0x00ffffff-no-rj",
         name: "조코딩 JoCoding",
